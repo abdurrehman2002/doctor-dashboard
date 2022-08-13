1773be017d2443c9d32b450d7c69a6e1403914a4
abdurrehman2002 <abdurrehman.ptf786@gmail.com>
2022-08-13 20:45:59 +0500

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+    return (
+        <div>
+            <button className='btn'><span>+</span>Sign up</button>
+        </div>
+    );
+}
+
+export default Button;
---
+++ src/pages/Signup.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import Button from '../components/Button';
+
+
+const Signup= () =>{
+    return(
+        <div>
+            <Button/>
+        </div>
+    )
+}
+export default Signup;
